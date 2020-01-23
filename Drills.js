fd4db1cc320804697886d869698b6c022811048c
stephenschab <sschab689@gmail.com>
2020-01-23 10:25:24 -0700

    answered #3

--- Drills.js
+++ Drills.js
@@ -14,6 +14,7 @@ function no2() {
   console.log(arr);
 }
 no2();
-// length = 1, capacity = 3, address = 0
-// length = 6, capacity = 12, address = 3
-
+// 2.1) length = 1, capacity = 3, address = 0
+// 2.2) length = 6, capacity = 12, address = 3
+// 3.1 a) length = 3, capacity = 12, address = 3
+// 3.1 b) The address stays the same for the pop method but the length of the array is decreased. The data is still there but no longer being referenced by the array methods.
